6c57e32fc1ed35b23855066b215035f8aa326c06
Roman Wicky <rwickyvd@gmail.com>
2021-03-15 22:42:54 -0400

    setup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 # Chrome
-browser = webdriver.Chrome("C:/Users/rwick/PycharmProjects/seleniumtesting/chromedriver.exe")
+browser = webdriver.Chrome("C:/Users/rwick/PycharmProjects/seleniumfun/chromedriver.exe")
 
 #Random page
 browser.get("https://www.bestbuy.com/site/apple-watch-series-6-gps-44mm-space-gray-aluminum-case-with-black-sport-band-space-gray/6215931.p?skuId=6215931")
